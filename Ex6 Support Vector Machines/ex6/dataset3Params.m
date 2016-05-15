13505a8ec8539fb87c3b79c62bfdfd317d3dfb30
Thomas Tong <hello@thomastong.me>
2016-05-15 18:27:07 +0800

    ex6 submit

--- Ex6 Support Vector Machines/ex6/dataset3Params.m
+++ Ex6 Support Vector Machines/ex6/dataset3Params.m
@@ -23,11 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+steps_vec = [0.01 0.03 0.1 0.3 1 3 10 30];
+errors = zeros(length(steps_vec), length(steps_vec));
 
+for i = 1:length(steps_vec)
+    for j = 1:length(steps_vec)
+        
+        C = steps_vec(i);
+        sigma = steps_vec(j);
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        errors(i, j) = error;
+    end
+end
 
-
-
-
+[i,j] = find(errors==min(errors(:)));
+C = steps_vec(i);
+sigma = steps_vec(j);
 
 % =========================================================================
 
--- Ex6 Support Vector Machines/ex6/processEmail.m
+++ Ex6 Support Vector Machines/ex6/processEmail.m
@@ -97,14 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
+    m = size(vocabList,1);
+    for i=1:m
+        if strcmp(str,vocabList(i)) ==1
+            word_indices = [word_indices;i;];
+        end
+    end
 
     % =============================================================
 
