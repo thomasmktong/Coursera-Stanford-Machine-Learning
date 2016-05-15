b780aad7a94bc8fc39a7f190a80935b5d7d7b828
Thomas Tong <hello@thomastong.me>
2016-05-15 22:04:50 +0800

    ex8 submit

--- Ex8 Anomaly Detection and Recommender Systems/ex8/selectThreshold.m
+++ Ex8 Anomaly Detection and Recommender Systems/ex8/selectThreshold.m
@@ -23,17 +23,14 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
-
-
-
-
-
-
-
-
-
-
-
+    ypre = pval < epsilon;
+    tp = sum(ypre(yval==1));
+    fp = sum(ypre(yval==0));
+    fn = sum(yval==1) - tp;
+    
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
+    F1 = 2 * prec * rec / (prec + rec);
 
     % =============================================================
 
